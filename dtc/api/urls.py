80d2fca5c5c1fb042d7eb35a83692455be5e7cda
Larisa Kosareva <kos.lv@yandex.com>
2022-05-12 14:53:04 +0200

    post route person added

--- dtc/api/urls.py
+++ dtc/api/urls.py
@@ -5,10 +5,11 @@ from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('', views.getRoutes),
-    path('obtain-token/', obtain_auth_token),
+    # path('obtain-token/', obtain_auth_token),
     
     path('people/', views.getPeople),
-    path('people/<str:pk>/', views.getPerson),
+    path('people/add/', views.addPerson),
+    path('people/<str:pk>/', views.personDetails),
     
     path('events/', views.getEvents),
     path('events/<str:pk>/', views.getEvent)
--- dtc/api/views.py
+++ dtc/api/views.py
@@ -1,9 +1,10 @@
 
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.permissions import IsAuthenticated
 from .serializers import PersonSerializer, EventSerializer
 from root.models import Person, Event
+from rest_framework import status
 
 
 @api_view(['GET'])
@@ -40,16 +41,35 @@ def getRoutes(request):
     return Response(routes)
 
 @api_view(['GET'])
+# @permission_classes([IsAuthenticated])
 def getPeople(request):
+    print("DATA+++++>:", request.data)
     people = Person.objects.all()
     serializer = PersonSerializer(people, many=True)
     return Response(serializer.data)
 
-@api_view(['GET'])
-def getPerson(request, pk):
+@api_view(['POST'])
+# @permission_classes([IsAuthenticated])
+def addPerson(request):
+    print("DATA+++++>:", request.data)
+    serializer = PersonSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+   
+
+@api_view(['GET', 'DELETE', 'PUT'])
+def personDetails(request, pk):
     person = Person.objects.get(id=pk)
-    serializer = PersonSerializer(person, many=False)
-    return Response(serializer.data)
+    if request.method == 'GET':
+        serializer = PersonSerializer(person, many=False)
+        return Response(serializer.data)
+    
+    # elif request.method == 'PUT':
+
+
+
 
 @api_view(['GET'])
 def getEvents(request):
